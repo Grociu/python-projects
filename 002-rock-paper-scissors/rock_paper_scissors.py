d410da787b527f6dcb5cf4235298d16e0ebe9518
Grociu <jan.grottel@gmail.com>
2020-12-05 13:33:20 +0100

    Implement rps function and tests

--- 002-rock-paper-scissors/rock_paper_scissors.py
+++ 002-rock-paper-scissors/rock_paper_scissors.py
@@ -1,2 +1,41 @@
+import tkinter as tk
+from enum import Enum
+import random
+
+
+# Define data types for rock, paper, scissors and outcomes.
+class Choice(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
+class Outcome(Enum):
+    UNDETERMINED = 0
+    PLAYER1_WIN = 1
+    PLAYER2_WIN = 2
+    DRAW = 3
+
 def rock_paper_scissors(choice_player_1, choice_player_2):
+    if not (
+        isinstance(choice_player_1, Choice) and 
+        isinstance(choice_player_2, Choice)
+    ):
+        raise TypeError("Invalid Input") 
+    if choice_player_1 == choice_player_2:
+        return Outcome.DRAW
+    if (
+        choice_player_1 == Choice.ROCK and choice_player_2 == Choice.SCISSORS
+     ) or (
+        choice_player_1 == Choice.SCISSORS and choice_player_2 == Choice.PAPER
+     ) or (
+        choice_player_1 == Choice.PAPER and choice_player_2 == Choice.ROCK
+     ):
+        return Outcome.PLAYER1_WIN
+    else:
+        return Outcome.PLAYER2_WIN
+
+def main():
     pass
+
+if __name__ == '__main__':
+    main()
--- 002-rock-paper-scissors/testmodule.py
+++ 002-rock-paper-scissors/testmodule.py
@@ -1,59 +1,61 @@
 import unittest
-import rock_paper_scissors
+import rock_paper_scissors as r_p_s
 
 
 # functions used
-rps = rock_paper_scissors.rock_paper_scissors
-placeholder = None #change this to the correct types
+rps = r_p_s.rock_paper_scissors
+Choice = r_p_s.Choice
+Outcome = r_p_s.Outcome
 
 class TestRPS(unittest.TestCase):
 
     # tests for correct input
     def test_string_input(self):
-        "A string should raise an error"
-        self.assertRaises(rps("scissors","rock"), TypeError)
+        """A string should raise an error"""
+        self.assertRaises(TypeError, rps, "scissors", "rock")
 
     def test_partial_string_input(self):
-        "A string should raise an error"
-        self.assertRaises(rps(placeholder,"rock"), TypeError)
+        """A string should raise an error"""
+        self.assertRaises(TypeError, rps, Choice.ROCK, "rock")
 
     def test_integer_input(self):
-        "A string should raise an error"
-        self.assertRaises(rps(1,0), TypeError)
+        """A string should raise an error"""
+        self.assertRaises(TypeError, rps, 1, 0)
 
     def test_boolean_input(self):
-        "A string should raise an error"
-        self.assertRaises(rps(True, False), TypeError)
+        """A string should raise an error"""
+        self.assertRaises(TypeError, rps, True, False)
 
 
     # tests for correct outcomes
     def test_rps_outcome_1_pp(self):
         """ Paper Paper """
-        pass
+        self.assertIs(rps(Choice.PAPER, Choice.PAPER), Outcome.DRAW)
+
     def test_rps_outcome_2_ps(self):
         """ Paper Scissors """
-        pass
+        self.assertIs(rps(Choice.PAPER, Choice.SCISSORS), Outcome.PLAYER2_WIN)
     def test_rps_outcome_3_pr(self):
         """ Paper Rock """
-        pass
+        self.assertIs(rps(Choice.PAPER, Choice.ROCK), Outcome.PLAYER1_WIN)
     def test_rps_outcome_4_ss(self):
         """ Scissors Scissors """
-        pass
+        self.assertIs(rps(Choice.SCISSORS, Choice.SCISSORS), Outcome.DRAW)
     def test_rps_outcome_5_sp(self):
         """ Scissors Paper """
-        pass
+        self.assertIs(rps(Choice.SCISSORS, Choice.PAPER), Outcome.PLAYER1_WIN)
     def test_rps_outcome_6_sr(self):
         """ Scissors Rock """
-        pass
+        self.assertIs(rps(Choice.SCISSORS, Choice.ROCK), Outcome.PLAYER2_WIN)
     def test_rps_outcome_7_rr(self):
         """ Rock Rock """
-        pass
+        self.assertIs(rps(Choice.ROCK, Choice.ROCK), Outcome.DRAW)
     def test_rps_outcome_8_rp(self):
         """ Rock Paper """
-        pass
+        self.assertIs(rps(Choice.ROCK, Choice.PAPER), Outcome.PLAYER2_WIN)
     def test_rps_outcome_9_rs(self):
         """ Rock Scissors """
-        pass
+        self.assertIs(rps(Choice.ROCK, Choice.SCISSORS), Outcome.PLAYER1_WIN)
 
 
 if __name__ == '__main__':
