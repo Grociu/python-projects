17f64ef4e1f339c8aa6bcd77a6c8d7ea68332e34
Grociu <jan.grottel@gmail.com>
2020-12-08 15:56:15 +0100

    Make PlayerWindow class for player input

--- 002-rock-paper-scissors/rock_paper_scissors.py
+++ 002-rock-paper-scissors/rock_paper_scissors.py
@@ -1,7 +1,7 @@
-import tkinter as tk
-import random
 from enum import Enum
 from PIL import ImageTk, Image
+import random
+import tkinter as tk
 
 
 # Define data types for rock, paper, scissors and outcomes.
@@ -11,7 +11,7 @@ class Choice(Enum):
     SCISSORS = 3
 
 class Outcome(Enum):
-    UNDETERMINED = 0
+    UNDETERMINED = 1
     PLAYER1_WIN = 1
     PLAYER2_WIN = 2
     DRAW = 3
@@ -41,56 +41,104 @@ def rock_paper_scissors(choice_player_1, choice_player_2):
         return Outcome.PLAYER2_WIN
 
 
-def p1_rock():
-    global p1_choice
+class PlayerWindow(tk.LabelFrame):
 
-    p1_made_choice()
-    p1_choice = Choice.ROCK
-    pass
+    def __init__(self, shortcuts, master, **kwargs):
+        super().__init__(master, **kwargs)
+        # Binds the keyboard keys to specific button functions
+        master.bind(shortcuts[0], self.rock)
+        master.bind(shortcuts[0].lower(), self.rock)
+        master.bind(shortcuts[1], self.paper)
+        master.bind(shortcuts[1].lower(), self.paper)
+        master.bind(shortcuts[2], self.scissors)
+        master.bind(shortcuts[2].lower(), self.scissors)
+        self.shortcuts = shortcuts
+        self.setup_frame()
+
+    def setup_frame(self):
+        """
+        Sets up the frame at game start, ready to receive player input.
+        """
+        self.choice = None
+        
+        self.rock_picture = tk.Label(self, image=rock_img)
+        self.rock_picture.grid(row=1, column=0)
+
+        self.rock_button = tk.Button(
+            self, text=f"rock ({self.shortcuts[0]})", command=self.rock,
+            relief='sunken'
+            )
+        self.rock_button.grid(row=2, column=0)
+        
+        self.paper_picture = tk.Label(self, image=paper_img)
+        self.paper_picture.grid(row=1, column=2)
+
+        self.paper_button = tk.Button(
+            self, text=f"paper ({self.shortcuts[1]})", command=self.paper,
+            relief='sunken'
+            )
+        self.paper_button.grid(row=2, column=2)
+
+        self.scissors_picture = tk.Label(self, image=scissors_img)
+        self.scissors_picture.grid(row=3, column=1)
+        
+        self.scissors_button = tk.Button(
+            self, text=f"scissors ({self.shortcuts[2]})",
+            command=self.scissors, relief='sunken'
+            )
+        self.scissors_button.grid(row=4, column=1)
+
+        self.choice_label = tk.Label(self, text="No choice made")
+        self.choice_label.grid(row=5, column=1, pady=30)
 
 
-def p1_paper():
-    global p1_choice
-
-    p1_made_choice()
-    p1_choice = Choice.PAPER
-    pass
-
-
-def p1_scissors():
-    p1_made_choice()
-    p1_choice = Choice.SCISSORS
-    pass
-
-def p1_made_choice():
-    """ Player 1 made a choice.
-    Disables buttons. 
-    Displays a message if waiting for player 2 choice. (?)
-    Display game result if both made choice. (?)
-    """
-    global p1_rock_button
-    global p1_paper_button
-    global p1_scissors_button
-    global p1_choice_label
-
-    p1_rock_button = tk.Button(
-        p1_frame, text="rock (Q)", command=p1_rock, state="disabled"
+    def choice_made(self):
+        """
+        Player pushed any button.
+        Update the frames after any button is pushed, disables the buttons.
+        """
+        self.rock_button = tk.Button(
+            self, text=f"rock ({self.shortcuts[0]})",
+            relief='sunken', command=self.rock,
+            state="disabled"
         )
-    p1_rock_button.grid(row=2, column=0)
+        self.rock_button.grid(row=2, column=0)
 
-    p1_paper_button = tk.Button(
-        p1_frame, text="paper (W)", command=p1_paper, state="disabled"
+        self.paper_button = tk.Button(
+            self, text=f"paper ({self.shortcuts[1]})", 
+             relief='sunken', command=self.paper,
+            state="disabled"
         )
-    p1_paper_button.grid(row=2, column=2)
+        self.paper_button.grid(row=2, column=2)
 
-    p1_scissors_button = tk.Button(
-        p1_frame, text="scissors (A)", command=p1_scissors, state="disabled"
+        self.scissors_button = tk.Button(
+            self, text=f"scissors ({self.shortcuts[2]})",
+            relief='sunken', command=self.scissors,
+            state="disabled"
         )
-    p1_scissors_button.grid(row=4, column=1)
+        self.scissors_button.grid(row=4, column=1)
 
-    p1_choice_label.grid_forget()
-    p1_choice_label = tk.Label(p1_frame, text="P1 has chosen!")
-    p1_choice_label.grid(row=5, column=1, pady=30)
+        self.choice_label.grid_forget()
+        self.choice_label = tk.Label(self, text="Player has chosen!")
+        self.choice_label.grid(row=5, column=1, pady=30)
+    
+
+    def rock(self, _event=None):  # _event=None is needed for the keybindings
+        if not self.choice:
+            self.choice_made()
+            self.choice = Choice.ROCK
+
+
+    def paper(self, _event=None):
+        if not self.choice:
+            self.choice_made()
+            self.choice = Choice.PAPER
+
+
+    def scissors(self, _event=None):
+        if not self.choice:
+            self.choice_made()
+            self.choice = Choice.SCISSORS
 
 
 def main():
@@ -114,27 +162,16 @@ scissors_img = ImageTk.PhotoImage(
     Image.open("002-rock-paper-scissors/resources/scissors.jpg")
     )
 
-# Player One Game Box
-p1_frame = tk.LabelFrame(root, text="Player 1", width=300, height=400)
-p1_frame.grid(row=0, column=0)
+# Define keyboard shortcuts for P1 and P2
+p1_shortcuts = ["Q", "W", "A"]
+p2_shortcuts = ["O", "P", "L"]
 
-p1_rock_picture = tk.Label(p1_frame, image=rock_img)
-p1_rock_picture.grid(row=1, column=0)
-p1_rock_button = tk.Button(p1_frame, text="rock (Q)", command=p1_rock)
-p1_rock_button.grid(row=2, column=0)
+# Setup the Game window
+p1 = PlayerWindow(p1_shortcuts, root, text="Player 1", width=300, height=400)
+p1.grid(row=1, column=1)
 
-p1_paper_picture = tk.Label(p1_frame, image=paper_img)
-p1_paper_picture.grid(row=1, column=2)
-p1_paper_button = tk.Button(p1_frame, text="paper (W)", command=p1_paper)
-p1_paper_button.grid(row=2, column=2)
-
-p1_scissors_picture = tk.Label(p1_frame, image=scissors_img)
-p1_scissors_picture.grid(row=3, column=1)
-p1_scissors_button = tk.Button(p1_frame, text="scissors (A)", command=p1_scissors)
-p1_scissors_button.grid(row=4, column=1)
-
-p1_choice_label = tk.Label(p1_frame, text="No choice made")
-p1_choice_label.grid(row=5, column=1, pady=30)
+p2 = PlayerWindow(p2_shortcuts, root, text="Player 2", width=300, height=400)
+p2.grid(row=1, column=2)
 
 # Main Tkinter Loop
 root.mainloop()
