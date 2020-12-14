7575f2f69cebdbd16029e7796060ab86c5e89829
Grociu <jan.grottel@gmail.com>
2020-12-14 20:33:20 +0100

    Add basic tkinter elements and remove subclasses

--- 003-dice-roller/dice_roller.py
+++ 003-dice-roller/dice_roller.py
@@ -12,56 +12,58 @@ class Dice(object):
     def __init__(self, sides):
         self.sides = sides
         self.result = 0
+        self.name = f"d{sides}"
 
     def roll(self):
         self.result = random.randint(1, self.sides)
 
 
-class D4(Dice):
-    """ A d4 """
-    def __init__(self):
-        super().__init__(4)
+# Global Variable for the Dice Pool
+dice_pool = []
 
 
-class D6(Dice):
-    """ A d6 """
-    def __init__(self):
-        super().__init__(6)
-
-
-class D8(Dice):
-    """ A d8 """
-    def __init__(self):
-        super().__init__(8)
-
-
-class D10(Dice):
-    """ A d10 """
-    def __init__(self):
-        super().__init__(10)
-
-
-class D12(Dice):
-    """ A d12 """
-    def __init__(self):
-        super().__init__(12)
-
-
-class D20(Dice):
-    """ A d20 """
-    def __init__(self):
-        super().__init__(20)
-
-
-class D100(Dice):
-    """ A d100 """
-    def __init__(self):
-        super().__init__(100)
-
-
-def main():
+# Functions
+def add_die(sides):
+    """ Adds a Dice(sides) object into  the dice pool to be rolled. """
     pass
 
 
+# Setup of tkinter
+root = tk.Tk()
+root.title("Dice roller!")
+root.geometry("330x600")
+
+# Label for adding dice
+top_label = tk.Label(root, text="Add dice to the dicepool:")
+top_label.grid(row=0, column=0, columnspan=7, padx=10, pady=10, sticky='w')
+
+# Dice buttons
+d4_button = tk.Button(root, text='d4', command=lambda: add_die(4))
+d4_button.grid(row=1, column=0, padx=(20, 8))
+
+d6_button = tk.Button(root, text='d6', command=lambda: add_die(6))
+d6_button.grid(row=1, column=1, padx=8)
+
+d8_button = tk.Button(root, text='d8', command=lambda: add_die(8))
+d8_button.grid(row=1, column=2, padx=8)
+
+d10_button = tk.Button(root, text='d10', command=lambda: add_die(10))
+d10_button.grid(row=1, column=3, padx=8)
+
+d12_button = tk.Button(root, text='d12', command=lambda: add_die(12))
+d12_button.grid(row=1, column=4, padx=8)
+
+d20_button = tk.Button(root, text='d20', command=lambda: add_die(20))
+d20_button.grid(row=1, column=5, padx=8)
+
+d100_button = tk.Button(root, text='d100', command=lambda: add_die(100))
+d100_button.grid(row=1, column=6, padx=(8, 20))
+
+# Label for adding dice
+dice_pool_label = tk.Label(root, text="Current dicepool:")
+dice_pool_label.grid(
+    row=2, column=0, columnspan=7, padx=10, pady=10, sticky='w'
+    )
+
 if __name__ == '__main__':
-    main()
+    root.mainloop()
