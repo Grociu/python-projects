8a09d16244e3ac2348049f04acca7a4795967049
Grociu <jan.grottel@gmail.com>
2020-12-17 13:11:56 +0100

    Add code for 003

--- 003-dice-roller/dice_roller.py
+++ 003-dice-roller/dice_roller.py
@@ -25,10 +25,12 @@ class DicePool(object):
     Attributes:
         .pool - a list of Dice objects in the DicePool
         .counter - a dictionary with a specific count of each die
-
+        .order - a sorted list of used dice types
     Methods:
         add() - adds a die to DicePool
         repr() - represents the DicePool as a string
+        empty() - removes all Dice from the pool
+        roll_all() - rolls all the dice in the pool
     """
     def __init__(self):
         self.pool = []
@@ -46,6 +48,16 @@ class DicePool(object):
             else:
                 self.counter[die.name] = 1
 
+    def empty(self):
+        """ Empties the dice pool """
+        self.pool.clear()
+        self.counter.clear()
+
+    def roll_all(self):
+        """ Rolls all the dice in the pool """
+        for die in self.pool:
+            die.roll()
+
     def __repr__(self):
         """ This also orders the dice in ascending order """
         return "  ".join(
@@ -61,27 +73,52 @@ dice_pool = DicePool()
 
 # Functions
 def add_die(sides):
-    """ Adds a Dice(sides) object into  the dice pool to be rolled. """
+    """ Adds a Dice(sides) object into the dice pool to be rolled. """
     global dice_pool
     global dice_pool_window
 
     dice_pool.add(Dice(sides))
 
-    dice_pool_window.grid_forget()
-    dice_pool_window = tk.Label(
-        root,
-        background='white',
-        text=repr(dice_pool)
+    dice_pool_window['text'] = repr(dice_pool)
+
+
+def reset():
+    """ Clears the Dice Pool, and the label """
+    global dice_pool
+    global dice_pool_window
+
+    dice_pool.empty()
+
+    dice_pool_window['text'] = 'empty'
+
+
+def execute():
+    """ Executes a roll for the dice pool and updates the result label """
+    global dice_pool
+    global result_window
+
+    dice_pool.roll_all()
+
+    result_string = ""
+
+    for die_type in dice_pool.counter.keys():
+        rolls = [die.result for die in dice_pool.pool if die.name == die_type]
+        result_string += (
+            f"{dice_pool.counter[die_type]}{die_type}: "
+            f"{sorted(rolls)}, Sum: {sum(rolls)}\n"
         )
-    dice_pool_window.grid(
-        row=3, column=0, columnspan=7, ipady=30, padx=10, sticky='we'
+
+    if result_string:
+        result_string += (
+            f"\nTotal Sum: {sum(die.result for die in dice_pool.pool)}"
         )
+        result_window['text'] = result_string
 
 
 # Setup of tkinter
 root = tk.Tk()
 root.title("Dice roller!")
-root.geometry("330x600")
+root.geometry("330x430")
 
 # Label for adding dice
 top_label = tk.Label(root, text="Add dice to the dicepool:")
@@ -118,7 +155,31 @@ dice_pool_label.grid(
 # Dice pool window
 dice_pool_window = tk.Label(root, background='white', text='empty')
 dice_pool_window.grid(
-    row=3, column=0, columnspan=7, padx=10, ipady=30, sticky='we'
+    row=3, column=0, columnspan=7, padx=10, ipady=20, sticky='we'
+    )
+
+# Reset and Roll buttons
+reset_button = tk.Button(root, text="Clear", command=reset)
+reset_button.grid(row=4, column=1, ipadx=15, pady=15, columnspan=2)
+
+roll_button = tk.Button(root, text="Roll", command=execute)
+roll_button.grid(row=4, column=4, ipadx=15, pady=10, columnspan=2)
+
+# Label for the result window
+result_label = tk.Label(root, text="Results of the last roll:")
+result_label.grid(
+    row=5, column=0, columnspan=7, padx=10, pady=10, sticky='w'
+    )
+
+# Result window
+result_window = tk.Label(
+    root,
+    background='white',
+    text='Add dice to the dice pool and press the ROLL button',
+    height=9
+    )
+result_window.grid(
+    row=6, column=0, columnspan=7, padx=10, sticky='we'
     )
 
 if __name__ == '__main__':
