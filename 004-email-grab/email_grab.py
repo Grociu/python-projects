354a4e6364cc1e12bfe303771de803d6edf145b4
Grociu <jan.grottel@gmail.com>
2020-12-21 12:07:59 +0100

    Add main logic loop for button

--- 004-email-grab/email_grab.py
+++ 004-email-grab/email_grab.py
@@ -31,9 +31,27 @@ def grab_emails(text):
     return re.findall(email_match, text)
 
 
+def execute():
+    global feedback
+    global output_window
+    """ Function that runs when the get Run button is pushed """
+    output_window.delete('1.0', 'end')
+    url = input_window.get()
+    try:
+        soup = cook_soup(url)
+        text = soup.get_text()
+        emails = grab_emails(text)
+        output_window.insert('end', ", \n".join(emails))
+        feedback['fg'] = 'green'
+        feedback['text'] = 'Executed correctly'
+    except requests.exceptions.MissingSchema:
+        feedback['fg'] = 'red'
+        feedback['text'] = 'Request error, Invalid URL\nTry adding http://'
+
+
 root = tk.Tk()
 root.title("E-mail Address Grabber")
-root.geometry("600x200")
+root.geometry("600x300")
 
 input_window_label = tk.Label(root, text="Paste URL here:")
 input_window_label.grid(row=0, column=0, padx=5)
@@ -41,23 +59,20 @@ input_window_label.grid(row=0, column=0, padx=5)
 input_window = tk.Entry(root, width=70)
 input_window.grid(row=0, column=1, padx=5)
 
-# submit_button_label = tk.Label(root, text="Run Script to get e-mail addresses")
-# submit_button_label.grid(row=2, column=0)
-
-submit_button = tk.Button(root, text="Run")
+submit_button = tk.Button(root, text="Run", command=execute)
 submit_button.grid(row=0, column=2, padx=5, ipadx=4)
 
 feedback_label = tk.Label(root, text="Progress:")
 feedback_label.grid(row=4, column=0)
 
-feedback = tk.Label(root, text="")
-feedback.grid(row=5, column=0)
+feedback = tk.Label(root, text="", fg="red")
+feedback.grid(row=5, column=1, columnspan=2)
 
 output_label = tk.Label(root, text="Output:")
 output_label.grid(row=6, column=0)
 
-output_window = tk.Label(root, text="")
-output_window.grid(row=7, column=0)
+output_window = tk.Text(root, height=12, width=65)
+output_window.grid(row=7, column=0, columnspan=3, padx=10)
 
 
 if __name__ == '__main__':
